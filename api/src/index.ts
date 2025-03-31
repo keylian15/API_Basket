b847fac8aaaf9b34d80d10354f12fd3a04ca39dc
keylian15 <keylian59555@gmail.com>
2025-03-31 22:29:42 +0200

    docs(teams): ajout de la docs

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,17 @@
 import express from "express";
 import { teamsRouter } from "./teams/teams.router";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
 
 export const app = express();
 const port = 3000;
+
+// Charger la spécification Swagger
+const swaggerDocument = YAML.load(path.join(__dirname, "./swagger.yaml"));
+
+// Serveur Swagger UI à l'adresse /api-docs
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 

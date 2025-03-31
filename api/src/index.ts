33ed19ac5b6738b7cf7af6ed7b16818fe87fdfcf
keylian15 <keylian59555@gmail.com>
2025-03-31 22:18:39 +0200

    feat(teams): ajouts des endpoints get/teams et get/teams/:id

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,3 +1,15 @@
 import express from "express";
+import { teamsRouter } from "./teams/teams.router";
 
 export const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use(teamsRouter);
+
+export const server = app.listen(port);
+
+export function stopServer() {
+  server.close();
+}
---
+++ api/src/teams/teams.router.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { getTeam, getTeams } from "./teams.controller";
+
+export const teamsRouter = Router();
+
+teamsRouter.get("/teams", getTeams);
+teamsRouter.get("/teams/:id", getTeam);

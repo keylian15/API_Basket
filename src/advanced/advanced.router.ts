24b3d41e329afd2480ca7e265c81bdef1166680b
keylian15 <keylian59555@gmail.com>
2025-04-03 09:15:06 +0200

    Fix/endpoints (#15)
    * fix(bdd): modification about advanced and match
    
    * fix(advanced): add paramater on endpoints

--- src/advanced/advanced.router.ts
+++ src/advanced/advanced.router.ts
@@ -3,4 +3,4 @@ import { getAdvanced, getAdvanceds } from "./advanced.controller";
 
 export const advancedRouter = Router();
 advancedRouter.use("/advanceds", getAdvanceds);
-advancedRouter.use("/advanced/:id", getAdvanced);
+advancedRouter.use("/advanced/:id/:saison/:abr", getAdvanced);

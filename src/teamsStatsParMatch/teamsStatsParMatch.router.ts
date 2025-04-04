9d0ab257aa82495e8f091b3efe428d4719ebf519
keylian15 <keylian59555@gmail.com>
2025-04-04 11:13:28 +0200

    feat(teamState): crud

--- src/teamsStatsParMatch/teamsStatsParMatch.router.ts
+++ src/teamsStatsParMatch/teamsStatsParMatch.router.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import {
+  createTeamStatsParMatch,
+  deleteTeamStatsParMatch,
   getTeamStatsParMatch,
   getTeamsStatsParMatch,
+  updateTeamStatsParMatch,
 } from "./teamsStatsParMatch.controller";
+import { verifyJWT } from "../commun/commun.middleware ";
 
 export const teamStatsParMatch = Router();
 
@@ -10,4 +14,19 @@ teamStatsParMatch.get("/teamsStatsParMatch", getTeamsStatsParMatch);
 teamStatsParMatch.get(
   "/teamStatsParMatch/:saison/:abr/:qualif",
   getTeamStatsParMatch
+);
+teamStatsParMatch.post(
+  "/teamStatsParMatch",
+  verifyJWT,
+  createTeamStatsParMatch
+);
+teamStatsParMatch.patch(
+  "/teamStatsParMatch/:saison_param/:abr_equipe_param/:qualif_param",
+  verifyJWT,
+  updateTeamStatsParMatch
+);
+teamStatsParMatch.delete(
+  "/teamStatsParMatch/:saison_param/:abr_equipe_param/:qualif_param",
+  verifyJWT,
+  deleteTeamStatsParMatch
 );

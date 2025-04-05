1669fb44dccbe8476b06168979e6436dc38a50a2
keylian15 <keylian59555@gmail.com>
2025-04-05 19:58:15 +0200

    fix(classement): add a verification

--- src/classement/classement.router.ts
+++ src/classement/classement.router.ts
@@ -27,69 +27,74 @@ import { verifyJWT } from "../commun/commun.middleware ";
 
 export const classementRouter = Router();
 
-classementRouter.get("/classement/joueurs/contre", getClsJsContre);
+classementRouter.get("/classement/joueurs/contre", verifyJWT, getClsJsContre);
 classementRouter.get(
   "/classement/joueurs/contre/:id_joueur/:saison",
-
+  verifyJWT,
   getClJsContre
 );
-classementRouter.get("/classement/joueurs/fg", getClsJsFg);
+classementRouter.get("/classement/joueurs/fg", verifyJWT, getClsJsFg);
 classementRouter.get(
   "/classement/joueurs/fg/:id_joueur/:saison",
-
+  verifyJWT,
   getClJsFg
 );
-classementRouter.get("/classement/joueurs/ft", getClsJsFt);
+classementRouter.get("/classement/joueurs/ft", verifyJWT, getClsJsFt);
 classementRouter.get(
   "/classement/joueurs/ft/:id_joueur/:saison",
-
+  verifyJWT,
   getClJsFt
 );
 classementRouter.get(
   "/classement/joueurs/interceptions",
-
+  verifyJWT,
   getClsJsInterception
 );
 classementRouter.get(
   "/classement/joueurs/interceptions/:id_joueur/:saison",
-
+  verifyJWT,
   getClJsInterception
 );
-classementRouter.get("/classement/joueurs/minutes", getClsJsMinute);
+classementRouter.get("/classement/joueurs/minutes", verifyJWT, getClsJsMinute);
 classementRouter.get(
   "/classement/joueurs/minutes/:id_joueur/:saison",
+  verifyJWT,
   getClJsMinute
 );
 
-classementRouter.get("/classement/joueurs/passes", getClsJsPasse);
+classementRouter.get("/classement/joueurs/passes", verifyJWT, getClsJsPasse);
 classementRouter.get(
   "/classement/joueurs/passes/:id_joueur/:saison",
-
+  verifyJWT,
   getClJsPasse
 );
-classementRouter.get("/classement/joueurs/points", getClsJsPt);
+classementRouter.get("/classement/joueurs/points", verifyJWT, getClsJsPt);
 classementRouter.get(
   "/classement/joueurs/points/:id_joueur/:saison",
-
+  verifyJWT,
   getClJsPt
 );
-classementRouter.get("/classement/joueurs/rebonds", getClsJsRebond);
+classementRouter.get("/classement/joueurs/rebonds", verifyJWT, getClsJsRebond);
 classementRouter.get(
   "/classement/joueurs/rebonds/:id_joueur/:saison",
-
+  verifyJWT,
   getClJsRebond
 );
-classementRouter.get("/classement/joueurs/tir", getClsJsTir);
+classementRouter.get("/classement/joueurs/tir", verifyJWT, getClsJsTir);
 classementRouter.get(
   "/classement/joueurs/tir/:id_joueur/:saison",
-
+  verifyJWT,
   getClJsTir
 );
-classementRouter.get("/classement/joueurs/trois_pts", getClsJs3Pt);
+classementRouter.get("/classement/joueurs/trois_pts", verifyJWT, getClsJs3Pt);
 classementRouter.get(
   "/classement/joueurs/trois_pts/:id_joueur/:saison",
-
+  verifyJWT,
   getClJs3Pt
 );
-classementRouter.get("/classement/teams/pts", getClsTmPt);
-classementRouter.get("/classement/teams/pts/:saison/:abr_equipe", getClTmPt);
+classementRouter.get("/classement/teams/pts", verifyJWT, getClsTmPt);
+classementRouter.get(
+  "/classement/teams/pts/:saison/:abr_equipe",
+  verifyJWT,
+  getClTmPt
+);
